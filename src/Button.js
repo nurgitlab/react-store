2909493f44cea3c13e846275b665e0e0b0c8a96c
nurgitlab <nurbek.math@gmail.com>
2021-07-31 22:21:01 +0600

    some info props

--- src/Button.js
+++ src/Button.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import classNames from "classnames";
 
-class Button extends React.Component{
-  render() {
-    return (
-      <button
-        className={`button ${this.props.outline ? 'button--outline' : ''}`}
-      >
-        {this.props.children}
-      </button>
-    )
-  }
+
+function Button(props) {
+  return (
+    <button
+      className={classNames('button', {
+        'button--outline': props.outline,
+      })}
+    >
+      {props.children}
+    </button>
+  )
 }
-
 export default Button;
