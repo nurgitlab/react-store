77eb10622aec4146b49f7bd0850c7ac9b50610fc
nurgitlab <nurbek.math@gmail.com>
2021-08-13 15:26:44 +0600

    shavablock component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Header } from './components';
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
@@ -6,11 +6,21 @@ import { Route } from "react-router-dom";
 
 
 function App() {
+  const [shavas, setShavas] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/db.json')
+      .then((resp)=>resp.json())
+      .then(json=>{
+      setShavas(json.shavas)
+    })
+  },[])
+
   return (
     <div className="wrapper">
       <Header/>
       <div className="content">
-        <Route path="/" component={Home} exact/>
+        <Route path="/" render={()=><Home items={shavas}/>} exact/>
         <Route path="/cart" component={Cart} exact/>
       </div>
     </div>
