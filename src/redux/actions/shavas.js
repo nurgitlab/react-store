35ebc3a5a99ab747ccaae94e21e0cf397fc04a15
nurgitlab <nurbek.math@gmail.com>
2021-08-24 23:49:17 +0600

    sorting by price and showing all items fixed

--- src/redux/actions/shavas.js
+++ src/redux/actions/shavas.js
@@ -10,10 +10,15 @@ export const fetchShavas = (sortBy, category) => (dispatch) => {
   dispatch({
     type: 'SET_LOADED',
     payload: false,
-  })
-  axios.get(`http://localhost:3001/shavas?category=${category}`).then(({data}) => {
-    dispatch(setShavas(data));
   });
+  axios
+    .get(`http://localhost:3001/shavas?${
+      category !== null ? `category=${category}` : ''
+    }&_sort=${sortBy}&_order=desc`,
+    )
+    .then(({data}) => {
+      dispatch(setShavas(data));
+    });
 };
 
 export const setShavas = (items) => ({
