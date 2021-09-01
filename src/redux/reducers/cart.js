ba7a949ecaf8b84bfa41b290586f9fe628e90e4d
nurgitlab <nurbek.math@gmail.com>
2021-09-01 17:55:03 +0300

    bug with shava put in cart fixed

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -6,18 +6,20 @@ const initialState = {
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_SHAVA_CART':
+    case 'ADD_SHAVA_CART': {
+      const newItems = {
+        ...state.items,
+        [action.payload.id]: !state.items[action.payload.id]
+          ? [action.payload]
+          : [...state.items[action.payload.id], action.payload],
+      };
+
       return {
         ...state,
-        items: {
-          [action.payload.id]: !state.items[action.payload.id]
-            ? [action.payload]
-            : [
-              ...state.items[action.payload.id],
-              action.payload,
-            ],
-        },
+        items: newItems,
+        totalCount: [].concat.apply([], Object.values(newItems)).length,
       };
+    }
 
     default:
       return state;
